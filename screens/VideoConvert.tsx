c0324370924cdfe7c4df75952d22714b2ca85282
Mohammad Hatif Osmani <66199079+hatif03@users.noreply.github.com>
2024-03-17 19:58:31 +0530

    Getting videos from gallery.

--- screens/VideoConvert.tsx
+++ screens/VideoConvert.tsx
@@ -4,6 +4,7 @@ import React from 'react'
 type Props = {}
 
 const VideoConvert = (props: Props) => {
+  console.log(props.route.params.selectedImg)
   return (
     <View>
       <Text>VideoConvert</Text>
--- screens/VideoSelect.tsx
+++ screens/VideoSelect.tsx
@@ -1,14 +1,45 @@
 import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React, {useState} from 'react'
 import AppButton from '../components/AppButton'
 import { Ionicons } from '@expo/vector-icons'
+import * as ImagePicker from 'expo-image-picker'
+import { useNavigation } from '@react-navigation/native'
 
 type Props = {}
 
 const VideoSelect = (props: Props) => {
+
+  const navigate = useNavigation()
+
+  const [image, setImage] = useState(null);
+
+  const pickImage = async () => {
+    // No permissions request is necessary for launching the image library
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Videos,
+      allowsEditing: false,
+      allowsMultipleSelection: false,
+      quality: 1,
+    });
+
+    console.log(result);
+
+    if (!result.canceled) {
+      setImage(result.assets[0].uri);
+      navigate.navigate('VideoConvert', {selectedImg: image})
+    } else{
+      console.log("cancelled")
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <AppButton onPress={() => console.log("pressed")}>
+      <AppButton 
+        onPress={() => {
+          console.log("pressed")
+          pickImage()
+        }}
+      >
         <Ionicons name="cloud-upload-outline" size={24} color="#2E6EEE" />
         <Text style={styles.btnTitle}>Choose Video</Text>
       </AppButton>
